103e1e27d3a5341930a37674bbbdf430e209b5e7
Adnan Ali <48458068+adi-tsvet@users.noreply.github.com>
2023-04-25 04:25:56 -0400

    ui changes

--- appusers/models.py
+++ appusers/models.py
@@ -16,42 +16,52 @@ STATUS_CHOICES = [
     ('C', 'Canceled')
 ]
 
+
 class Course(models.Model):
     c_name = models.CharField(max_length=100)
     c_code = models.CharField(max_length=50, unique=True, null=False)
 
     def __str__(self):
-        return self.c_code
+        return self.c_name
+
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     ums_id = models.CharField(max_length=20, unique=True, null=False)
     courses = models.ManyToManyField(Course, related_name='students')
-    no_shows=models.IntegerField(max_length=1,default=0)
+    no_shows = models.IntegerField(max_length=1, default=0)
 
     def __str__(self):
         return self.user.username
+
     def booked_slots(self):
         return self.booked_slots.all()
+
 
 class Tutor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     courses = models.ManyToManyField(Course, related_name='tutors')
+
     def availabilities(self):
         return self.availabilities.all()
 
     def __str__(self):
         return self.user.username
-class Availability (models.Model):
+
+
+class Availability(models.Model):
     tutor = models.ForeignKey(Tutor, on_delete=models.CASCADE, null=True, blank=True, related_name='availabilities')
     date = models.DateField()
     timeblock = models.CharField(max_length=1, choices=TIMEBLOCK_CHOICES)
-    booked_by = models.ForeignKey(Student, on_delete=models.SET_NULL, null=True, blank=True, related_name='booked_slots')
+    booked_by = models.ForeignKey(Student, on_delete=models.SET_NULL, null=True, blank=True,
+                                  related_name='booked_slots')
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     semester = models.CharField(max_length=8, null=True)
+
     def __str__(self):
         return f"{self.tutor} - {self.date} - {self.get_timeblock_display()} with {self.booked_by} is {self.status}"
+
     def check_semester(self):
         today = self.date
         if today.month >= 1 and today.month <= 4:
@@ -67,16 +77,17 @@ class Availability (models.Model):
     def save(self, *args, **kwargs):
         self.check_semester()
         super().save(*args, **kwargs)
-    
-class Department (models.Model):
+
+
+class Department(models.Model):
     d_name = models.CharField(max_length=100)
+
     def __str__(self):
         return self.d_name
 
+
 class SemesterDates(models.Model):
-    name=models.CharField(max_length=20,primary_key=True)
-    startDate=models.DateField(null=False)
-    endDate=models.DateField(null=False)
-    currentSemester=models.BooleanField(default=False)
-
-
+    name = models.CharField(max_length=20, primary_key=True)
+    startDate = models.DateField(null=False)
+    endDate = models.DateField(null=False)
+    currentSemester = models.BooleanField(default=False)
