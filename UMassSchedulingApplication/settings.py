905cab9ff463005d62df930496c739d10fc755b7
Adnan Ali <48458068+adi-tsvet@users.noreply.github.com>
2023-05-04 03:59:29 -0400

    minor changes

--- UMassSchedulingApplication/settings.py
+++ UMassSchedulingApplication/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9%i9xn&^399)_7ht_3i%rxz7ylc8h*d(f@0&hx-u@l$$h+_p^r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 # Internationalization
